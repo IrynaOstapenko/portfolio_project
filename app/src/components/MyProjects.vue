480e99d38e0f2bd8701e117ccce68119b25983f2
IrynaOstapenko <52035358+IrynaOstapenko@users.noreply.github.com>
2022-05-12 17:15:45 +0200

    Changes

--- app/src/components/MyProjects.vue
+++ app/src/components/MyProjects.vue
@@ -4,8 +4,12 @@
     <div class="projects-container">
         <div v-for="project in projects" class="projects-container__item">
             <div>{{ project.title }}</div>
-            <img :src="project.image.asset.url" alt="project-image" class="projects-container__image">
-            <span class="project-image__popup">{{ project.description }}</span>
+            <div class="projects-container__image-container">
+                <img :src="project.image.asset.url" alt="project-image" class="image-container__image">
+                <div class="image-container__overlay">
+                    <div class="image-container__overlay-description">{{ project.description }}</div>
+                </div>                
+            </div>            
             <div class="projects-container__links">
                 <a :href="project.gitHubLink" aria-label="link-to-github"><img src="/images/github-icon.png" alt="github-icon"></a>
                 <a :href="project.netlifyLink" aria-label="link-to-netlify"><img src="/images/netlify-icon.png" alt="netlify-icon"></a>
@@ -15,9 +19,22 @@
     
 </template>
 
-<script>
+<script>    
     import Navigation from '../components/Navigation.vue';
-    import WeatherForecast from '../components/WeatherForecast.vue'
+    import WeatherForecast from '../components/WeatherForecast.vue';
+
+    /* Sanity client not working*/
+
+    // import sanityClient from '@sanity/client';
+
+	// const sanity = sanityClient({
+	// 	projectId: 'hepvg5nh',
+	// 	dataset: 'production',
+	// 	apiVersion: '2022-05-10', 
+	// 	useCdn: false 
+	// });
+    
+    /*..............*/
 
     export default {
         components: {
@@ -26,11 +43,14 @@
         },
         data() {
             return {
-                 projects: []
+                loading: true,
+                result: null,
+                projects: []
             }           
         },
 
         async created() {
+            /*Ordinary sanity link is working properly */
             const sanity_query_url = 'https://hepvg5nh.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type%20%3D%3D%20%24type%5D%20%7B%0A%20%20_id%2C%0A%20%20title%2C%0A%20%20slug%2C%0A%20%20description%2C%0A%20%20gitHubLink%2C%0A%20%20netlifyLink%2C%0A%20%20image%20%7B%0A%20%20%20asset-%3E%20%7B%0A%20%20%20%20url%0A%20%20%20%7D%0A%20%20%7D%0A%7D&%24type=%22project%22'
 
 		    const sanity_response = await fetch(sanity_query_url);
@@ -38,6 +58,17 @@
 
 		    this.projects = result;
             console.log(this.projects);
+
+            /*Second part of the sanity client that's not working*/
+
+            // const query = `*[_type == $type]`
+			// const params = { type: 'project' };
+
+			// this.projects = await sanity.fetch(query, params);
+			// this.loading = false;
+            // console.log(this.projects);
+
+            /*.......*/
         }
     }
 
@@ -61,41 +92,45 @@
         margin-bottom: 80px;
     }
 
-    .projects-container__image {
+    .projects-container__image-container {
         width: 60%;
-        height: auto;
+        height: 30%;
+        position: relative;
+        cursor: pointer;
     }
 
-    .project-image__popup {
-        visibility: hidden;
-        width: 200px;
-        background-color: #555;
-        color: #fff;
+    .image-container__image {
+        width: 100%;
+        height: 100%;
+        object-fit: contain;
+    }
+
+    .image-container__overlay {
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        height: 100%;
+        width: 100%;
+        /* opacity: 0; */
+        overflow: hidden;
+        transform: scale(0);
+        transition: .4s ease;
+        background-color: var(--nav-color);
+    }
+
+    .projects-container__image-container:hover .image-container__overlay {
+        transform: scale(1);
+    }
+
+    .image-container__overlay-description {
+        color: white;
+        font-size: 20px;
+        position: absolute;
+        top: 50%;
+        transform: translateY(-50%);
         text-align: center;
-        border-radius: 6px;
-        padding: 8px 0;
-        position: absolute;
-        z-index: 1;
-        bottom: 125%;
-        left: 50%;
-        margin-left: -80px;
-    }
-
-    .project-image__popup:after {
-        content: "";
-        position: absolute;
-        top: 100%;
-        left: 50%;
-        margin-left: -5px;
-        border-width: 5px;
-        border-style: solid;
-        border-color: #555 transparent transparent transparent;
-    }
-
-    .project-image__popup:hover {
-        visibility: visible;
-        -webkit-animation: fadeIn 1s;
-        animation: fadeIn 1s;
+        padding: 1em;
     }
 
     .projects-container__links {
